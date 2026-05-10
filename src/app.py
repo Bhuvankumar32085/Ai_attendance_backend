ecaa7b3d272c82539a088f1412222fa1a6b7d7fd
bhuvan <bhuvankumar66666@gmail.com>
2026-05-10 17:57:30 +0530

    first commit

--- src/app.py
+++ src/app.py
@@ -19,4 +19,10 @@ CORS(
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True
+    )
