f894dace7c2d42f4a80370494555d836234ad7ba
bhuvan <bhuvankumar66666@gmail.com>
2026-05-10 20:55:34 +0530

    first commit

--- src/configs/db.py
+++ src/configs/db.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("SUPABASE_URL =", os.getenv("SUPABASE_URL"))
+print("SUPABASE_KEY =", os.getenv("SUPABASE_KEY"))
+
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
