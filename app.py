6f3b5a237b201850280bbe6defeaf78abede6121
bhuvan <bhuvankumar66666@gmail.com>
2026-05-13 12:51:21 +0530

    first commit

--- app.py
+++ app.py
@@ -30,11 +30,14 @@ def home():
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5001))
 
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=True
-    )
+#     app.run(
+#         host="0.0.0.0",
+#         port=port,
+#         debug=True
+#     )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
